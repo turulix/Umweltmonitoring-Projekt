87ee9a224fda38208fca408f0775379c3f385335
Tim <tigga01@online.de>
2024-06-20 12:24:28 +0200

    Data Downloader

---
+++ data_types/tables.py
@@ -0,0 +1,32 @@
+from geoalchemy2 import Geometry
+from sqlalchemy import Column, String, ForeignKey, TIMESTAMP, REAL
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Box(Base):
+    __tablename__ = "boxes"
+    id = Column(String, primary_key=True)
+    model = Column(String)
+    name = Column(String)
+    location = Column(Geometry(geometry_type='POINT', srid=4326))
+    exposure = Column(String)
+
+
+class Sensor(Base):
+    __tablename__ = "sensors"
+    id = Column(String, primary_key=True)
+    box_id = Column(String, ForeignKey("boxes.id"))
+    icon = Column(String)
+    title = Column(String)
+    unit = Column(String)
+    sensor_type = Column(String)
+
+
+class Data(Base):
+    __tablename__ = "data"
+    box_id = Column(String, ForeignKey("boxes.id"), primary_key=True)
+    sensor_id = Column(String, ForeignKey("sensors.id"), primary_key=True)
+    timestamp = Column(TIMESTAMP, primary_key=True)
+    value = Column(REAL)
--- download_data.py
+++ download_data.py
@@ -3,13 +3,21 @@ import os
 from datetime import datetime, timedelta, timezone
 import json
 import requests
+from geoalchemy2.shape import from_shape
+from sqlalchemy.orm import sessionmaker
 from tqdm import tqdm
 import dotenv
 import pandas as pd
+import geopandas as pgd
+from shapely.geometry import Point
+
+from data_types.tables import Base, Box, Sensor, Data
 
 dotenv.load_dotenv()
 
 client = sqlalchemy.create_engine(os.environ.get("DATABASE_URL"))
+Base.metadata.create_all(client)
+session = sessionmaker(bind=client)()
 
 station_ids = [
     "65e8d93acbf5700007f920ca",  # (Leipziger Straße)
@@ -21,16 +29,33 @@ station_ids = [
 with client.connect() as conn:
     for station_id in station_ids:
         station_details = requests.get(f"https://api.opensensemap.org/boxes/{station_id}").json()
-        station_df = pd.DataFrame.from_dict(station_details, orient="index").T[
-            ["_id", "model", "name", "currentLocation", "exposure"]]
-        # Convert the currentLocation to a geopandas POINT
-        station_df["currentLocation"] = station_df["currentLocation"].apply(
-            lambda x: f"POINT ({x['coordinates'][0]} {x['coordinates'][1]})")
-        station_df = station_df.rename(columns={"_id": "id", "currentLocation": "location"}).T.T
-        station_df.to_sql("boxes", conn, if_exists="append", index=False)
+
+        session.add(Box(
+            id=station_id,
+            model=station_details["model"],
+            name=station_details["name"],
+            location=from_shape(Point([station_details["currentLocation"]["coordinates"][0],
+                                       station_details["currentLocation"]["coordinates"][1]]), srid=4326),
+            exposure=station_details["exposure"]
+        ))
+
+        session.commit()
+
         for sensor in station_details["sensors"]:
             sensor_id = sensor["_id"]
             title = sensor["title"]
+
+            session.add(Sensor(
+                id=sensor_id,
+                box_id=station_id,
+                icon=sensor["icon"],
+                title=title,
+                unit=sensor["unit"],
+                sensor_type=sensor["sensorType"]
+            ))
+
+            session.commit()
+
             # Get the last two years of measurements for each sensor in 48 hour intervals
             now = datetime.now()
             sensor_data = []
@@ -38,11 +63,25 @@ with client.connect() as conn:
                 delta = timedelta(days=1)
                 measurements_req = requests.get(
                     f"https://api.opensensemap.org/boxes/{station_id}/data/{sensor_id}?"
-                    f"from-date={(now - delta * i).isoformat('T')}Z&to-date={(now - delta * (i - 1)).isoformat('T')}Z").json()
+                    f"from-date={(now - delta * i).isoformat('T')}Z&to-date={(now - delta * (i - 1)).isoformat('T')}Z"
+                ).json()
                 sensor_data.extend(measurements_req)
-                if len(measurements_req) == 0:
-                    # No more data available, we can terminate early
-                    break
+
+            # Store Sensor Data To Database
+            processed_data = []
+            for measurement in sensor_data:
+                # '2024-06-20T10:04:48.775Z'
+                processed_data.append(Data(
+                    box_id=station_id,
+                    sensor_id=sensor_id,
+                    timestamp=datetime.strptime(measurement["createdAt"], "%Y-%m-%dT%H:%M:%S.%fZ").replace(
+                        tzinfo=timezone.utc),
+                    value=float(measurement["value"])
+                ))
+
+            session.add_all(processed_data)
+            session.commit()
+
             # Store Sensor Data To File
-            with open(f"{station_id}_{title}.json", "w") as f:
-                f.write(str(json.dumps(sensor_data, indent=4)))
+            # with open(f"./data/{station_id}_{title}.json", "w") as f:
+            #     f.write(str(json.dumps(sensor_data, indent=4)))
